225f19cd70efa60dbd731cd1b8046f493ae70350
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-21 17:32:29 -0400

    feat: dashboard

---
+++ website/components/Shared/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="border-t-16 border-primary flex justify-between px-24 py-12">
+    <NuxtLink to="/"
+      ><img src="@/assets/img/sensai.svg" alt="Logo" class="w-28"
+    /></NuxtLink>
+    <div class="space-x-12">
+      <NuxtLink to="/" class="font-serif">home</NuxtLink>
+      <NuxtLink to="/dashboard" class="font-serif">workouts</NuxtLink>
+      <NuxtLink to="/account" class="font-serif">my account</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'SharedNavbar',
+});
+</script>
