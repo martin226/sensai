8f6ded530e9915aa92db4a9730c5398683fc2cd8
xcfrg <30675315+xcfrg@users.noreply.github.com>
2022-05-22 09:39:03 -0400

    fix: workout route bugs
    Signed-off-by: xcfrg <30675315+xcfrg@users.noreply.github.com>

--- backend/backend/routes/workout.py
+++ backend/backend/routes/workout.py
@@ -13,8 +13,9 @@ workout_collection = db["workouts"]
 users_collection = db["users"]
 
 default_log = {
-    "name": "",
     "id": "",
+    "name": "",
+    "workout_id": "",
     "completed": False,
     "created_at": None,
     "completed_at": None,
@@ -40,10 +41,10 @@ def workout_get(id_):
     return make_response(jsonify({"workout": requested_workout}), 200)
 
 
-@workout.route("/start/<id_>")
+@workout.route("/start/<workout_id>")
 @jwt_required()
-def workout_start(id_):
-    requested_workout = workout_collection.find_one({"_id": ObjectId(id_)})
+def workout_start(workout_id):
+    requested_workout = workout_collection.find_one({"_id": ObjectId(workout_id)})
     requested_workout["_id"] = str(requested_workout["_id"])
     requested_workout["id"] = requested_workout.pop("_id")
     current_user = get_jwt_identity()
@@ -51,7 +52,8 @@ def workout_start(id_):
     if user_from_db:
         new_log = default_log.copy()
         new_log["name"] = requested_workout["name"]
-        new_log["id"] = requested_workout["id"]
+        new_log["id"] = len(user_from_db["workout_log"])
+        new_log["workout_id"] = requested_workout["id"]
         new_log["created_at"] = int(datetime.now().timestamp())
         users_collection.update_one(
             filter={"username": current_user},
@@ -61,24 +63,23 @@ def workout_start(id_):
     return make_response(jsonify({"workout": requested_workout}), 200)
 
 
-@workout.route("/end/<id_>")
+@workout.route("/end/<int:id_>")
 @jwt_required()
 def workout_end(id_):
-    requested_workout = workout_collection.find_one({"_id": ObjectId(id_)})
-    requested_workout["_id"] = str(requested_workout["_id"])
-    requested_workout["id"] = requested_workout.pop("_id")
     current_user = get_jwt_identity()
     user_from_db = users_collection.find_one({"username": current_user})
+    print(user_from_db)
     if user_from_db:
         workout_log = user_from_db["workout_log"]
         for (idx, workout) in enumerate(workout_log):
+            print("a")
+            print(id_, idx, workout["id"])
             if workout["id"] == id_:
-                workout_log: workout[idx]["completed_at"] = int(
-                    datetime.now().timestamp()
-                )
+                workout_log[idx]["completed_at"] = int(datetime.now().timestamp())
+                workout_log[idx]["completed"] = True
                 break
         users_collection.update_one(
             {"username": current_user}, {"$set": {"workout_log": workout_log}}
         )
 
-    return make_response(jsonify({"workout": requested_workout}), 200)
+    return make_response(jsonify({"message": "Ended workout"}), 200)
