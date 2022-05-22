520300f0b4e7581ef1b9c8830144144406c56a0b
xcfrg <30675315+xcfrg@users.noreply.github.com>
2022-05-22 01:11:23 -0400

    feat: workout routes
    Signed-off-by: xcfrg <30675315+xcfrg@users.noreply.github.com>

--- backend/backend/routes/workout.py
+++ backend/backend/routes/workout.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, jsonify, make_response
+from datetime import datetime
+
 from bson.objectid import ObjectId
+from flask import Blueprint, jsonify, make_response
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from ..database import mongo
 
@@ -7,9 +10,19 @@ workout = Blueprint("workout", __name__)
 
 db = mongo["sensai"]
 workout_collection = db["workouts"]
+users_collection = db["users"]
+
+default_log = {
+    "name": "",
+    "id": "",
+    "completed": False,
+    "created_at": None,
+    "completed_at": None,
+}
 
 
 @workout.route("/list", methods=["GET"])
+@jwt_required()
 def workout_list():
     workouts = list(workout_collection.find())
     for workout in workouts:
@@ -19,8 +32,53 @@ def workout_list():
 
 
 @workout.route("/<id_>")
+@jwt_required()
 def workout_get(id_):
     requested_workout = workout_collection.find_one({"_id": ObjectId(id_)})
     requested_workout["_id"] = str(requested_workout["_id"])
     requested_workout["id"] = requested_workout.pop("_id")
+    return make_response(jsonify({"workout": requested_workout}), 200)
+
+
+@workout.route("/start/<id_>")
+@jwt_required()
+def workout_start(id_):
+    requested_workout = workout_collection.find_one({"_id": ObjectId(id_)})
+    requested_workout["_id"] = str(requested_workout["_id"])
+    requested_workout["id"] = requested_workout.pop("_id")
+    current_user = get_jwt_identity()
+    user_from_db = users_collection.find_one({"username": current_user})
+    if user_from_db:
+        new_log = default_log.copy()
+        new_log["name"] = requested_workout["name"]
+        new_log["id"] = requested_workout["id"]
+        new_log["created_at"] = int(datetime.now().timestamp())
+        users_collection.update_one(
+            filter={"username": current_user},
+            update={"$push": {"workout_log": new_log}},
+        )
+
+    return make_response(jsonify({"workout": requested_workout}), 200)
+
+
+@workout.route("/end/<id_>")
+@jwt_required()
+def workout_end(id_):
+    requested_workout = workout_collection.find_one({"_id": ObjectId(id_)})
+    requested_workout["_id"] = str(requested_workout["_id"])
+    requested_workout["id"] = requested_workout.pop("_id")
+    current_user = get_jwt_identity()
+    user_from_db = users_collection.find_one({"username": current_user})
+    if user_from_db:
+        workout_log = user_from_db["workout_log"]
+        for (idx, workout) in enumerate(workout_log):
+            if workout["id"] == id_:
+                workout_log: workout[idx]["completed_at"] = int(
+                    datetime.now().timestamp()
+                )
+                break
+        users_collection.update_one(
+            {"username": current_user}, {"$set": {"workout_log": workout_log}}
+        )
+
     return make_response(jsonify({"workout": requested_workout}), 200)
